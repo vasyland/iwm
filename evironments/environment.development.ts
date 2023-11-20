188bfe2ca9fd45e1694116f890b5089ed5d9fe2e
vasyland <vasyland@gmail.com>
2023-11-20 17:14:18 -0500

    project setup

--- evironments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8586',
-  urls: {
-    api: '/api/',
-    scenarios: 'api/scenarios',
-    decision: 'api/decision-data'
-  }
-};
