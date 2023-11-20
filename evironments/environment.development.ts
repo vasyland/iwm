52686635c6fb8307c6300e56a78e05d1999faf6d
vasyland <vasyland@gmail.com>
2023-11-20 07:18:11 -0500

    added environment files

---
+++ evironments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8586',
+  urls: {
+    api: '/api/',
+    scenarios: 'api/scenarios',
+    decision: 'api/decision-data'
+  }
+};
